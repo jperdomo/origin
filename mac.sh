711d44cf04278d341b935df2e88a38bc6070e78f
jperdomo <jperdomo3@gmail.com>
2023-02-26 15:33:03 -0500

    merge

---
+++ mac.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jperdomo/start/main/rhel/basics-fedora.sh)"
+curl -s https://raw.githubusercontent.com/jperdomo/start/main/mac/basics-install.sh | bash

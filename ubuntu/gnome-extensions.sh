2d40abc3324ef7082668a75b98c9cb9661be6769
jperdomo <jperdomo3@gmail.com>
2023-10-20 23:12:06 -0400

    gnome

---
+++ ubuntu/gnome-extensions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if gnome-extensions command is available
+if ! [ -x "$(command -v gnome-extensions)" ]; then
+  echo "Error: gnome-extensions command not found. Make sure you have the GNOME Shell Extensions package installed."
+  exit 1
+fi
+
+# UUIDs of the extensions
+runcat="run-cat@spoonless.github.io"
+dash_to_panel="dash-to-panel@jderose9.github.com"
+user_at_host="user_at_host@cmm.github.com"
+
+# RunCat
+gnome-extensions install "$runcat"
+gnome-extensions enable "$runcat"
+
+# Dash to Panel
+gnome-extensions install "$dash_to_panel"
+gnome-extensions enable "$dash_to_panel"
+
+# User @ Host
+gnome-extensions install "$user_at_host"
+gnome-extensions enable "$user_at_host"
+
+echo "RunCat, Dash to Panel, & User @ Host GNOME extensions installed and enabled."

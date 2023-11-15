382be652a359c4d78a38023e4e172a6947f993a6
jperdomo <jperdomo3@gmail.com>
2023-11-15 10:48:08 -0500

    more clean up

--- ubuntu/vscode-flatpak.sh
+++ ubuntu/vscode-flatpak.sh
@@ -6,9 +6,7 @@ sudo apt install flatpak -y
 ## Repo + Brave Install
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Brave
-flatpak install -y flathub com.brave.Browser
-## Default Browser
-xdg-settings set default-web-browser com.brave.Browser.desktop
+# VS Code
+flatpak install -y flathub com.visualstudio.code
 
 echo "Reboot Required!"

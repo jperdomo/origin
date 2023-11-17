cdc0b9f3a9d3e028ec0fbfb68d055b0ca059b108
jperdomo <jperdomo3@gmail.com>
2023-11-17 09:14:57 -0500

    old script rm

--- proxmox/install-tailscale.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
-apt update -y
-apt install -y tailscale
-tailscale up

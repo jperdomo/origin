29e8f84089d696e19d0d0a73a2099b13566cbeba
jperdomo <jperdomo3@gmail.com>
2023-11-18 09:13:19 -0500

    qemu + apt

--- ubuntu/install-cockpit.sh
+++ ubuntu/install-cockpit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Update
-sudo apt-get update -y
+sudo apt update
 
 # Install Cockpit
-sudo apt-get install cockpit podman cockpit-podman pcp cockpit-pcp -y
+sudo apt install -y cockpit podman cockpit-podman pcp cockpit-pcp
 
 # 45 Drives Repo + Cockpit Navigator
 curl -sSL https://repo.45drives.com/setup -o setup-repo.sh
 sudo bash setup-repo.sh
 
-sudo apt install cockpit-navigator -y
+sudo apt install -y cockpit-navigator
 
 # Enable and start the Cockpit service
 sudo systemctl enable --now cockpit.socket

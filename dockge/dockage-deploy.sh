2a92343630d26537709d750beb89ab2dccd735d8
jperdomo <jperdomo3@gmail.com>
2024-01-21 12:21:59 -0500

    dockge

---
+++ dockge/dockage-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check that we're root; if not, fail out
+if [[ $(whoami) != "root" ]]; then
+    echo "This script must be run as 'root' or with 'sudo' to function."
+    exit 1
+fi
+
+# Update + Install
+apt update -y
+
+# Create directories that store your stacks and stores Dockge's stack
+mkdir -p /opt/stacks /opt/dockge
+cd /opt/dockge
+
+# Download the compose.yaml
+curl https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml --output compose.yaml
+
+# Start the server
+docker compose up -d
+
+# If you are using docker-compose V1 or Podman
+# docker-compose up -d

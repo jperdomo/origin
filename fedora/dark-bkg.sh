53624afed300ed6261e7a4e12760883dbfc167b9
jperdomo <jperdomo3@gmail.com>
2024-08-31 14:01:17 -0400

    dark bkg

---
+++ fedora/dark-bkg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.background color-shading-type 'solid'
+gsettings set org.gnome.desktop.background picture-uri ''
+gsettings set org.gnome.desktop.background picture-uri-dark ''
+
+gsettings set org.gnome.desktop.background primary-color '#222222'
+gsettings set org.gnome.desktop.background secondary-color '#222222'

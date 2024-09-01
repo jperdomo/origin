893c804e95a1948c184236874c42965f118d703d
jperdomo <jperdomo3@gmail.com>
2024-09-01 11:34:34 -0400

    wip

--- proxmox/nvidia-proxmox-drivers-container-toolkit.sh
+++ proxmox/nvidia-proxmox-drivers-container-toolkit.sh
@@ -2,7 +2,13 @@
 
 apt install pve-headers-$(uname -r)
 
-curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | gpg –dearmor | tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
+#curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | gpg –dearmor | tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
+#curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | gpg --import
+#echo "nvidia-dep-11" > /etc/apt/sources.list.d/nvidia-drivers.list
+curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | gpg --import
+cat << EOF > /etc/apt/sources.list.d/nvidia-drivers.list
+deb http://download.nvidia.com/debian12 11.1 non-free
+EOF
 
 apt update
 
@@ -17,6 +23,8 @@ apt install nvidia-container-toolkit
 nvidia-ctk runtime configure –runtime=docker
 
 echo"
+
 ##############################################
    Reboot Required to complete installation
-##############################################"
+##############################################
+"

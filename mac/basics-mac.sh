d1fbd2705a03f4552c74e8c009dbcdcf3f129a86
jperdomo <jperdomo3@gmail.com>
2025-03-27 10:03:55 -0400

    mac update

--- mac/basics-mac.sh
+++ mac/basics-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Homebrew CLI
+# Homebrew
 brew install \
 git \
 gh \
@@ -9,37 +9,21 @@ htop \
 bmon \
 btop \
 neofetch \
-curl
-echo "| CLI Complete |"
-
-# Basics
+curl \
 brave-browser \
+zen-browser \
 cyberduck \
 remote-desktop-manager-free \
 utm \
 iperf \
 webstorm
-echo "| Cask Basics Complete |"
-
-#iterm2 \
-#arc \
-#termius \
-#element \
-#carbon-copy-cloner \
-#utm \
-#obsidian
-#vmware-fusion
-#firefox \
-#google-chrome \
-#visual-studio-code \
+echo "
+| Homebrew Basics Complete |
+"
 
 # MAS
 #mas - https://github.com/mas-cli/mas
 brew install mas
-
-# EQmac
-brew install --cask eqmac
-
 # RunCat
 mas install 1429033973
 #brew install --cask runcat-plugins-manager
@@ -47,8 +31,6 @@ mas install 1429033973
 mas install 1475387142
 # Magnet
 mas install 441258766
-# Todoist
-#mas install 585829637
 # NordVPN
 mas install 905953485
 # Blackmagic Disk Speed Test
@@ -60,11 +42,30 @@ mas install 404705039
 # DaisyDisk
 mas install 411643860
 
+echo "
+| App Store Basics Complete |
+"
+
+
+# Homebrew Archive
+#iterm2 \
+#arc \
+#termius \
+#element \
+#carbon-copy-cloner \
+#utm \
+#obsidian
+#vmware-fusion
+#firefox \
+#google-chrome \
+#visual-studio-code \
+#brew install --cask eqmac
+
+# MAS Archive
 # Infuse • Video Player
 #mas install 1136220934
-
-echo "| App Store Basics Complete |"
-
+# Todoist
+#mas install 585829637
 # Apollo - Broken
 #mas install 979274575
 # Parallels Desktop 

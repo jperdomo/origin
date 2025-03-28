a97626fe78dada18838c20fe2cf82ca51607752b
jperdomo <jperdomo3@gmail.com>
2025-03-28 15:51:57 -0400

    office

--- mac/office-install.sh
+++ mac/office-install.sh
@@ -4,21 +4,23 @@
 mas install 462054704
 #Microsoft Excel
 mas install 462058435
-#Microsoft PowerPoint
-mas install 462062816
 #OneDrive
 mas install 823766827
-#Microsoft OneNote
-mas install 784801555
-#Microsoft Remote Desktop 
-mas install 1295203466
-#Microsoft Outlook 
-mas install 985367838
+
+echo "| Microsoft Office |"
+
+#Archive
 #Teams
-brew install --cask microsoft-teams
+#brew install --cask microsoft-teams
 #Azure VPN Client
 #mas install  1553936137
 #Adobe Acrobat Pro
 #brew install --cask adobe-acrobat-pro
-
-echo "| Microsoft Office |"
+#Microsoft OneNote
+#mas install 784801555
+#Microsoft PowerPoint
+#mas install 462062816
+#Microsoft Remote Desktop
+#mas install 1295203466
+#Microsoft Outlook
+#mas install 985367838

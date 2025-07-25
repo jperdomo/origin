96b69819841b5f339e606c6db2391833370f1551
jperdomo <jperdomo3@gmail.com>
2025-07-24 20:49:40 -0400

    rm cloudinit

--- rocky/basics-rocky.sh
+++ rocky/basics-rocky.sh
@@ -24,10 +24,6 @@ fastfetch \
 iputils \
 nfs-utils
 
-#Speedtest
-#curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | bash
-#dnf install -y speedtest
-
 else
 echo "
 

e89aa8076767e7a34b339b284f41bbf9d21ea285
jperdomo <jperdomo3@gmail.com>
2025-08-07 11:56:59 -0400

    bazzite ollama

---
+++ bazzite/ollama-podman.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo setsebool -P container_use_devices=true
+
+#sudo podman run -it --name ollama   --replace  -p 127.0.0.1:11434:11434 -v ollama:/root/.ollama --device /dev/kfd --device /dev/dri   docker.io/ollama/ollama:rocm
+
+podman run -d --name ollama --label "io.containers.autoupdate=registry" --restart=unless-stopped -p 11434:11434 -v ollama:/root/.ollama --device /dev/kfd --device /dev/dri -e HSA_OVERRIDE_GFX_VERSION="10.3.0" docker.io/ollama/ollama:rocm
